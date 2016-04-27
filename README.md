7e1f224347b69629ea837efbda67e594524f4b53
Gregor G <design@gresak.net>
2016-04-27 11:19:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# GA for WP by GG
+
+Dead simple wordpress plugin that inserts GA tracking code to your wordpress site.
+
+## Instalation
+
+1. Clone to your wp-content/plugins/ directory
+2. Enable the plugin in your Wordpress administration
+3. Go to theme customization section
+4. Find the Google analytics section
+5. Insert your tracking id
+6. Save
+7. Enjoy the stats
+
+No code will be echoed unless the tracking code is present.
+
+## License
+
+GA for WP by GG simple plugin for inserting GA tracking code to your WP.
+
+Copyright 2016 by Gregor Gresak design@gresak.net
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; the version 2 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You can read the full text of this licence at the following link http://www.gnu.org/licenses/gpl-2.0.html
---
+++ ggga.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+Plugin Name: GA by GG
+Plugin URI: http://gresak.net
+Description: Simple plugin for google analytics
+Author: Gregor Grešak
+Version: 1.0
+Author URI: http://gresak.net
+*/
+
+new Ggga();
+
+class Ggga {
+
+	protected $tracking_id;
+
+	public function __construct() {
+		$this->tracking_id = get_theme_mod('tracking_id');
+		$this->set_actions();
+	}
+
+	public function customizer($customize) {
+		
+		$customize->add_section('google_analytics', array(
+			"title" => "Google Analytics",
+			"priority" => 100
+			));
+		$customize->add_setting('tracking_id',array("default"=>""));
+		$customize->add_control(
+			new WP_Customize_Control(
+				$customize,
+				'tracking_id',
+				array(
+					'label' => 'Google analytics tracking ID',
+					'section' => 'google_analytics',
+					'settings' => 'tracking_id'
+					)
+				)
+			);
+	}
+
+	public function print_ga() {
+		if(!empty($this->tracking_id)) {
+			echo "<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', '" . $this->tracking_id . "', 'auto');
+			  ga('send', 'pageview');
+
+			</script>";
+		}	
+	}
+
+	protected function set_actions() {
+		add_action('wp_head',array($this,'print_ga'));
+		add_action( 'customize_register', array($this,'customizer') );
+	}
+
+}
